b88a86edd1c016c80261776a4badf81653ae47aa
Richard <richardwells475@gmail.com>
2023-10-17 21:42:46 -0400

    fifth push

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,12 +3,53 @@ import "./Track.css";
 
 
 class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+  
+  renderAction() {
+      if (this.props.isRemoval) {
+        return (
+          <button className="Track-action" onClick={this.removeTrack}>
+            -
+          </button>
+          );
+      } else {
+        return (
+          <button className="Track-action" onClick={this.addTrack}>
+            +
+          </button>
+        );
+      }
+    }
+
+    addTrack() {
+      this.props.onAdd(this.props.track);
+    }
+
+    removeTrack() {
+      this.props.onRemove(this.props.track);
+    }
+
     render() {
       return (
         <div className="Track">
-          <div className="">
+          <div className="Track-information">
+            {/* <h3><!-- trackname will go here --></h3> */}
+            {/* <p><!-- track artist will go here --> | <!-- track album will go here --></p> */}
+            <h3>{this.props.track.name}</h3>
+            <p>
+              {this.props.track.artist} | {this.props.track.album}
+            </p>
+
+            {/* <h3>{this.props.track.playlistName}</h3>
+            <p>{this.props.track.playlistArtist} {this.props.track.playlistAlbum}</p> */}
 
           </div>
+          {/* <button className="Track-action"><!-- + or - will go here --></button> */}
+          {this.renderAction()}
         </div>
       );
     }
